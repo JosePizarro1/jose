5527b97040108f4d93438db133c4497365a2924c
JosePizarro1 <josepizarroarca@gmail.com>
2022-10-26 19:55:59 -0500

    intento 1 practica

--- hola.cpp
+++ hola.cpp
@@ -3,6 +3,9 @@
 int main () 
 {
     std::cout << "Hola, mundo";
+     std::cout <<endl;
      std::cout << "ADIOS MUNDO";
+        std::cout <<endl;
+     std::cout << "intento 1 xd";
     return 0;
 }
